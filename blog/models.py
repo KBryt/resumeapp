a44fbd5219f93379db418ef2347fd8dd3c4389fd
Bright_Effah <effahbright.eb@gmail.com>
2023-02-28 03:49:54 -0500

    Update

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    date = models.DateField()
+    description = models.TextField(max_length=300)
 
